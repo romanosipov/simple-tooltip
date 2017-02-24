fd4cb40c3eafce8c010ff21ae89d1cf76111d9b4
Роман Осипов <romanosipov@gmail.com>
2017-02-24 10:23:38 +0300

    Right and top toooltip modifications

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp   = require("gulp");
 var concat = require("gulp-concat");
+var reload = require("gulp-livereload");
 
 gulp.task("lib-js", function() {
   return gulp.src(['./bower_components/jquery/dist/jquery.min.js'])
@@ -9,17 +10,27 @@ gulp.task("lib-js", function() {
 
 gulp.task('js', function() {
   return gulp.src(['./src/js/simple-tooltip.js'])
-    .pipe(gulp.dest("./dist/js"));
+    .pipe(gulp.dest("./dist/js"))
+    .pipe(reload());
 });
 
 gulp.task('css', function() {
   return gulp.src(['./src/css/simple-tooltip.css'])
-    .pipe(gulp.dest("./dist/css"));
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(reload());
 });
 
 gulp.task("html", function() {
   return gulp.src("./src/html/*.html")
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./dist"))
+    .pipe(reload());
+});
+
+gulp.task('watch', function() {
+  reload.listen({start: true});
+  gulp.watch('src/js/**/*.js',     {cwd: './'}, ['js']);
+  gulp.watch('src/css/**/*.css',   {cwd: './'}, ['css']);
+  gulp.watch('src/html/**/*.html', {cwd: './'}, ['html']);
 });
 
 gulp.task("default", ['html', 'lib-js', 'js', 'css']);
--- src/js/simple-tooltip.js
+++ src/js/simple-tooltip.js
@@ -1,41 +1,58 @@
 $(document).ready(function() {
-        // Tooltip only Text
-        $('.masterTooltip').hover(function(){
-                // Hover over code
-                var title = $(this).attr('title');
-                $(this).data('tipText', title).removeAttr('title');
-                $('<p class="tooltip"></p>')
-                .text(title)
-                .appendTo('body')
-                .fadeIn('slow');
-        }, function() {
-                // Hover out code
-                $(this).attr('title', $(this).data('tipText'));
-                $('.tooltip').remove();
-        }).mousemove(function(e) {
-                var mousex = e.pageX + 20; //Get X coordinates
-                var mousey = e.pageY + 10; //Get Y coordinates
-                $('.tooltip')
-                .css({ top: mousey, left: mousex })
-        });
+  // Tooltip only Text
+  $('.masterTooltip').hover(function(){
+    // Hover over code
+    var title = $(this).attr('title');
+    $(this).data('tipText', title).removeAttr('title');
+    $('<p class="tooltip"></p>')
+    .text(title)
+    .appendTo('body')
+    .fadeIn('slow');
+  }, function() {
+    // Hover out code
+    $(this).attr('title', $(this).data('tipText'));
+    $('.tooltip').remove();
+  }).mousemove(function(e) {
+    var mousex = e.pageX + 20; //Get X coordinates
+    var mousey = e.pageY + 10; //Get Y coordinates
+    $('.tooltip').css({ top: mousey, left: mousex })
+  });
 
-        // Tooltip only Text
-        $('.masterTooltipUp').hover(function(){
-                // Hover over code
-                var title = $(this).attr('title');
-                $(this).data('tipText', title).removeAttr('title');
-                $('<p class="tooltip" style="position: absolute;"></p>')
-                .text(title)
-                .appendTo('body')
-                .fadeIn('slow');
-        }, function() {
-                // Hover out code
-                $(this).attr('title', $(this).data('tipText'));
-                $('.tooltip').remove();
-        }).mousemove(function(e) {
-                var mousex = e.pageX + 20; //Get X coordinates
-                var mousey = e.pageY - 50; //Get Y coordinates
-                $('.tooltip')
-                .css({ top: mousey, left: mousex })
-        });
+  $('.masterTooltipRight').hover(function(){
+    var title = $(this).attr('title');
+    $(this).data('tipText', title).removeAttr('title');
+    $('<p class="tooltip" style="position: absolute;"></p>')
+    .text(title)
+    .appendTo('body')
+    .fadeIn('slow');
+  }, function() {
+    $(this).attr('title', $(this).data('tipText'));
+    $('.tooltip').remove();
+  }).mousemove(function(e) {
+    var tooltip = $('.tooltip');
+
+    var x = e.target.offsetLeft + e.target.offsetWidth + 20;
+    var y = e.target.offsetTop - tooltip.height() / 2 - e.target.offsetHeight;
+
+    tooltip.css({left: x, top: y});
+  });
+
+  $('.masterTooltipUp').hover(function(){
+    var title = $(this).attr('title');
+    $(this).data('tipText', title).removeAttr('title');
+    $('<p class="tooltip" style="position: absolute;"></p>')
+    .text(title)
+    .appendTo('body')
+    .fadeIn('slow');
+  }, function() {
+    $(this).attr('title', $(this).data('tipText'));
+    $('.tooltip').remove();
+  }).mousemove(function(e) {
+    var tooltip = $('.tooltip');
+
+    var x = e.target.offsetLeft - tooltip.width() / 2;
+    var y = e.target.offsetTop - e.target.offsetHeight - tooltip.height() - 30;
+
+    tooltip.css({left: x, top: y});
+  });
 });
